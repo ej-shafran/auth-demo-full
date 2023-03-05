96d0a7ab84b69d3e18d6d398632077131a986562
ej-shafran <ejshafran@hlima.tech>
2023-03-05 21:14:43 +0200

    Create database (at last)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { TodoModule } from "./todo/todo.module";
 
 @Module({
-  imports: [TodoModule, ConfigModule.forRoot({
-    envFilePath: `.env.${process.env.NODE_ENV}`,
-    isGlobal: true
-  })],
+  imports: [
+    TodoModule,
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+
+      logging: process.env.TYPEORM_LOGGING === 'on',
+      synchronize: process.env.TYPEORM_SYNC === 'on',
+
+      entities: ["dist/**/*.entity{.ts,.js}"]
+    })
+  ],
   controllers: [],
   providers: [],
 })
--- server/src/todo/todo.service.ts
+++ server/src/todo/todo.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './todo.entity';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+    constructor(
+        @InjectRepository(Todo)
+        private readonly todoRepo: Repository<Todo>,
+    ) {}
+}
